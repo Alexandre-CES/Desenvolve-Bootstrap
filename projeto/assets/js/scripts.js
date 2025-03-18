f1573f98ab9a02496520c16dd11a17fffa75fe32
Alexandre-CES <alexandreces666@gmail.com>
2025-03-18 13:05:53 -0300

    modal portfolio

--- projeto/assets/js/scripts.js
+++ projeto/assets/js/scripts.js
@@ -6,3 +6,9 @@ const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstra
 //Tooltips
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+const exampleModal = document.getElementById('modalPortfolio');
+exampleModal.addEventListener('show.bs.modal', function(event)
+{
+    document.getElementById('modalImage').src = event.relatedTarget.getAttribute('data-img');
+});
